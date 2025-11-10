1226065174c97289494f1374031d6e94d1374058
fwzarash <fawwazarash@gmail.com>
2025-11-10 10:53:40 +0800

    Update GameManager.cs
    fawwaz 2

--- MyFirstGame/GameManager.cs
+++ MyFirstGame/GameManager.cs
@@ -52,7 +52,7 @@ namespace MyFirstGame
             // Store the graphics device so we can use it in Update()
             this.graphicsDevice = graphicsDevice;
             
-            // Load textures (fawwaz)
+            // Load textures (fawwaz 2)
             playerTexture = content.Load<Texture2D>("player_ship");
             // projectileTexture = content.Load<Texture2D>("bullet");
             // scoutTexture = content.Load<Texture2D>("alien_scout");
